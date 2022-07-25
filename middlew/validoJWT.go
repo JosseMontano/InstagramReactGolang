6836c5fb6ec44ad43f2638d8c94ae08bfd0e6ba1
JosseMontano <josemariazambranamontano123@gmail.com>
2022-07-25 15:49:26 -0400

    add a else

--- middlew/validoJWT.go
+++ middlew/validoJWT.go
@@ -7,9 +7,11 @@ func ValidoJWT(next http.HandlerFunc) http.HandlerFunc{
 	return func(w http.ResponseWriter, r *http.Request){
 		_, _, _, err := routers.ProcessToken(r.Header.Get("Authorization"))
 		if err != nil {
+			next.ServeHTTP(w,r)
+		} else{
 			http.Error(w, "Error en el Token ! " + err.Error(), http.StatusBadRequest)
 			return
 		}
-		next.ServeHTTP(w,r)
+	
 	}
 }
